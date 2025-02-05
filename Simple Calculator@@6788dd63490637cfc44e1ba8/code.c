6c49264995687f2882f7ff00ac56b013c342b7ed
kelden303 <kelden1878.becse24@chitkara.edu.in>
2025-02-05 21:15:43 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main{
+    int a, b;
+    char op;
+    scanf("%d %d", &a, &b, &op);
+
+    if(op == +){
+        printf("%d\n", a + b);
+    }
+    else if(op == -){
+        printf("%d", a - b);
+    }
+    else if(op == *){
+        printf("%d\n", a*b);
+    }
+    else if(op == /){
+        printf("%d", a/b);
+    }
+    else{
+        printf("Invalid Operand");
+    }
+    return 0;
+}
