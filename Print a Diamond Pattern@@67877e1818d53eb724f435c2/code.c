10f544e66db94435a430b0c9681b7b010f9f2c3e
kelden303 <kelden1878.becse24@chitkara.edu.in>
2025-02-10 11:41:37 +0530

    Solution to Print a Diamond Pattern

--- Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
+++ Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
@@ -1,30 +1,47 @@
 #include <stdio.h>
-int main(){    
+
+int main() {    
     int N, i, j, k;
+
+    // Take input for N
     scanf("%d", &N);
     
-    if(N%2 == 0){
-        printf("Enter a odd number!\n");
-        return 1;
+    // Debug: Print the input to verify
+    printf("You entered: %d\n", N);
+
+    // Check if N is odd
+    if (N % 2 == 0) {
+        printf("Enter an odd number!\n");
+        return 1;  // Exit if the input is even
     }
 
-    for(i = 1; i <= N; i+= 2){
-        for(k = 0; k < (N-i)/2; k++){
+    // Upper half of the diamond (including the middle row)
+    for (i = 1; i <= N; i += 2) {
+        // Print leading spaces
+        for (k = 0; k < (N - i) / 2; k++) {
             printf(" ");
         }
-        for(j = 0; j < i; j++){
+
+        // Print stars
+        for (j = 0; j < i; j++) {
             printf("*");
-        }    
-        printf("\n");
+        }
+        printf("\n");  // Move to the next line
     }
-    for(i = N -2; i > 0; i -= 2){
-        for(k = 0; k < (N-i)/2; k++){
+
+    // Lower half of the diamond (excluding the middle row)
+    for (i = N - 2; i > 0; i -= 2) {
+        // Print leading spaces
+        for (k = 0; k < (N - i) / 2; k++) {
             printf(" ");
         }
-        for(j = 0; j < i; j++){
+
+        // Print stars
+        for (j = 0; j < i; j++) {
             printf("*");
         }
-        printf("\n");
+        printf("\n");  // Move to the next line
     }
+
     return 0;
 }
