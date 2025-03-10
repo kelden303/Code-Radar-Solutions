f7f10bda20aa4dda3bc42eba0039261bec3dcd71
Kelden Lhatshog Jigsel <kelden1878.becse24@chitkara.edu.in>
2025-03-10 23:58:45 +0530

    Solution to Bubble Sort Implementation

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
+// Function to perform Bubble Sort  
 void bubbleSort(int arr[], int n) {
     int i, j, temp;
     for (i = 0; i < n - 1; i++) {
-        for (j = 0; j < n - i - 1; j++) { // Change 'i++' to 'j++'
+        for (j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
-                // Swap arr[j] and arr[j + 1]
+                // Swap arr[j] and arr[j+1]
                 temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
@@ -14,19 +15,27 @@ void bubbleSort(int arr[], int n) {
     }
 }
 
-main() {
-    int n, i;
+// Function to print the array  
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        if (i > 0) {
+            printf(" "); // Print space before next element
+        }
+        printf("%d", arr[i]);
+    }
+    printf("\n"); // New line after printing all elements
+}
+
+int main() {
+    int n;
     scanf("%d", &n);
     int arr[n];
-
-    for (i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
     
     bubbleSort(arr, n);
+    printArray(arr, n);
     
-    for (i = 0; i < n; i++) {
-        printf("%d ", arr[i]); // Added a space for better output formatting
-    }
-    return 0;
+    return 0;
 }
