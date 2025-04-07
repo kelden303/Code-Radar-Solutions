7aebbb57f52d4d53c6f4eb500746b6bf14305864
Kelden Lhatshog Jigsel <kelden1878.becse24@chitkara.edu.in>
2025-04-07 16:56:22 +0530

    Solution to Factorial Range

---
+++ Factorial Range@@67de00956ac3fec9afb90e10/code.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int factorialRange (int start, int end){
+    for(int i = start; i < end; i++){
+        if(i == 0){
+            return 1;
+        }
+        else{
+            return i * factorialRange(i - end);
+        }
+    }
+}
